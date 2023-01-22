5453d74bac62b8fc7012df8044f407dee755ffa9
keplerde <keplerde@gmail.com>
2023-01-22 11:33:04 +0100

    3 methods |slug|

--- myproject/getlink_app/urls.py
+++ myproject/getlink_app/urls.py
@@ -6,13 +6,16 @@ from .models import Page
 
 app_name = 'getlink'
 
+# Вариант 1.   '<slug>/'
+# Вариант 2.   '<str:slug>/'
+# Вариант 3.   '<slug:slug>/'
 
 # from my_site.views import ArticleListView
 
 
 urlpatterns = [
     path("slug/", PageListView.as_view(), name="list_slug"),
-    path('<slug>/', PageDetailView.as_view(), name="details")
+    path('<str:slug>/', PageDetailView.as_view(), name="details")
     # path('<slug:slug>/', PageDetailView.as_view(), name='detail_slug')
 
 ]
