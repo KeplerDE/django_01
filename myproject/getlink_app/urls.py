d5f200a913861ad8ee1ef06b3a63b1c6a960b738
Dennny <keplerde@gmail.com>
2023-01-24 04:02:04 +0100

    get_absolute_url

--- myproject/getlink_app/urls.py
+++ myproject/getlink_app/urls.py
@@ -1,7 +1,7 @@
 from django import urls
 from django.urls import path
 from django.urls.conf import re_path
-from .views import PageListView, PageDetailView, PageDetailNamespace, PageListNamespace
+from .views import PageListView, PageDetailView, PageDetailNamespace, PageListNamespace, PageListGetAbsoluteUrl, PageDetailGetAbsoluteUrl
 from .models import Page
 
 app_name = 'getlink'
@@ -19,6 +19,12 @@ urlpatterns = [
     # path('<slug:slug>/', PageDetailView.as_view(), name='detail_slug')
     # namespace
     path("slug/slug/", PageListNamespace.as_view(), name='view_list_ns'),
-    path("slug/<int:pk>/detail/", PageDetailNamespace.as_view, name='view_detail_ns'),
+    path("slug/<int:id>/detail/", PageDetailNamespace.as_view(), name='view_detail_ns'),
+
+    # get_absolute_url
+    path('absolute-path/list', PageListGetAbsoluteUrl.as_view(),
+         name='list_absolute_path'),
+    path('absolute-path/<int:id>/<str:slug>/detail/', PageDetailGetAbsoluteUrl.as_view(),
+         name='detail_absolute_path'),
 
 ]
--- myproject/getlink_app/views.py
+++ myproject/getlink_app/views.py
@@ -23,4 +23,21 @@ class PageListNamespace(ListView):
 class PageDetailNamespace(DetailView):
     model = Page
     template_name = "namespace/detail_ns.html"
+    context_object_name = 'details'          # обращение по контексту
+    # https://tutorial.djangogirls.org/en/extend_your_application/
+    #https://docs.djangoproject.com/en/4.1/ref/class-based-views/mixins-single-object/
+
+# переопределение get_absolute_url
+
+class PageListGetAbsoluteUrl(ListView):
+    model = Page
+    template_name = "get_absolute_url/list_get_absolute_url.html"
+    context_object_name = 'pg'
+
+
+class PageDetailGetAbsoluteUrl(DetailView):
+    model = Page
+    template_name = "get_absolute_url/detail_get_absolute_url.html"
+    context_object_name = 'details'
+
 
